2174b87e9f4a7e72760c6b47a30c007040cc28a0
KermX <KermitVIII@gmail.com>
2025-04-08 01:20:22 -0500

    Add support for targeting other players in crafting commands

--- src/main/java/me/kermx/prismaUtils/commands/crafting/CartographyTableCommand.java
+++ src/main/java/me/kermx/prismaUtils/commands/crafting/CartographyTableCommand.java
@@ -1,31 +1,59 @@
 package me.kermx.prismaUtils.commands.crafting;
 
 import me.kermx.prismaUtils.commands.BaseCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CartographyTableCommand extends BaseCommand {
 
     public CartographyTableCommand() {
-        super("prismautils.command.cartographytable", false, "/cartographytable");
+        super("prismautils.command.cartographytable", true, "/cartographytable");
     }
 
     @Override
     protected boolean onCommandExecute(CommandSender sender, String label, String[] args) {
-        if (args.length > 0) {
+        if (args.length > 1) {
             return false;
         }
-        Player player = (Player) sender;
-        Location location = player.getLocation();
-        player.openCartographyTable(location, true);
+
+        Player targetPlayer;
+
+        if (args.length == 0) {
+            if (!(sender instanceof Player)) {
+                return false;
+            }
+            targetPlayer = (Player) sender;
+        } else {
+            if (!(sender.hasPermission("prismautils.command.cartographytable.others"))) {
+                return false;
+            }
+            targetPlayer = sender.getServer().getPlayerExact(args[0]);
+            if (targetPlayer == null) {
+                return false;
+            }
+        }
+
+        Location location = targetPlayer.getLocation();
+        targetPlayer.openCartographyTable(location, true);
+
         return true;
     }
 
     @Override
     protected List<String> onTabCompleteExecute(CommandSender sender, String[] args) {
-        return super.onTabCompleteExecute(sender, args);
+        if (args.length == 1 && sender.hasPermission("prismautils.command.cartographytable.others")) {
+            return Bukkit.getOnlinePlayers().stream()
+                    .map(Player::getName)
+                    .filter(name -> name.toLowerCase().startsWith(args[0].toLowerCase()))
+                    .collect(Collectors.toList());
+        }
+        return new ArrayList<>();
     }
+
 }
--- src/main/java/me/kermx/prismaUtils/commands/crafting/EnderChestCommand.java
+++ src/main/java/me/kermx/prismaUtils/commands/crafting/EnderChestCommand.java
@@ -1,31 +1,59 @@
 package me.kermx.prismaUtils.commands.crafting;
 
 import me.kermx.prismaUtils.commands.BaseCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EnderChestCommand extends BaseCommand {
 
     public EnderChestCommand() {
-        super("prismautils.command.enderchest", false, "/enderchest");
+        super("prismautils.command.enderchest", true, "/enderchest");
     }
 
     @Override
     protected boolean onCommandExecute(CommandSender sender, String label, String[] args) {
-        if (args.length > 0) {
+        if (args.length > 1) {
             return false;
         }
-        Player player = (Player) sender;
-        Inventory enderChest = player.getEnderChest();
-        player.openInventory(enderChest);
+
+        Player targetPlayer;
+
+        if (args.length == 0) {
+            if (!(sender instanceof Player)) {
+                return false;
+            }
+            targetPlayer = (Player) sender;
+        } else {
+            if (!sender.hasPermission("prismautils.command.enderchest.others")) {
+                return false;
+            }
+
+            targetPlayer = Bukkit.getPlayerExact(args[0]);
+            if (targetPlayer == null) {
+                return false;
+            }
+        }
+
+        Inventory enderChest = targetPlayer.getEnderChest();
+        targetPlayer.openInventory(enderChest);
+
         return true;
     }
 
     @Override
     protected List<String> onTabCompleteExecute(CommandSender sender, String[] args) {
-        return super.onTabCompleteExecute(sender, args);
+        if (args.length == 1 && sender.hasPermission("prismautils.command.enderchest.others")) {
+            return Bukkit.getOnlinePlayers().stream()
+                    .map(Player::getName)
+                    .filter(name -> name.toLowerCase().startsWith(args[0].toLowerCase()))
+                    .collect(Collectors.toList());
+        }
+        return new ArrayList<>();
     }
 }
